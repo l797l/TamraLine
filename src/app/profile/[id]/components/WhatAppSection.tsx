6af1707709e4a9bf2595c82d0dee62f1f03e864b
l797l <ali.mahdi.67.99@gmail.com>
2026-08-14 00:31:07 +0300

    add link whatapp

--- src/app/profile/[id]/components/WhatAppSection.tsx
+++ src/app/profile/[id]/components/WhatAppSection.tsx
@@ -5,7 +5,7 @@ export default function WhatAppSection({
 }) {
   return (
     <a
-      href={`https://wa.me/${phoneNumber.replace(/\D/g, "")}`}
+      href={`https://wa.me/964${phoneNumber.replace(/\D/g, "").slice(1)}`}
       target="_blank"
       rel="noopener noreferrer"
 className="mt-4 flex w-full items-center justify-center gap-2 rounded-2xl bg-[#5B3F22] px-5 py-3 font-bold text-[#EFE1D1] shadow-md transition hover:bg-[#432E1A]"    >
