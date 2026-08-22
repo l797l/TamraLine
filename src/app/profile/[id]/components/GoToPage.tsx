54c86bfbab6a2108c85f3756e422d4857fa4c3af
l797l <ali.mahdi.67.99@gmail.com>
2026-08-23 01:06:01 +0300

    add buttom go to page?

--- src/app/profile/[id]/components/GoToPage.tsx
+++ src/app/profile/[id]/components/GoToPage.tsx
@@ -1,16 +1,20 @@
-import Link  from "next/link";
+import Link from "next/link";
 
 export default function GoToPage({
   Role,
 }: {
   Role: number;
 }) {
+  const isAdmin = Role === 3;
+
   return (
     <Link
-    href={Role === 3 ? "/admin/posts" :"/Posts"}
+      href={isAdmin ? "/admin/posts" : "/Posts"}
       className="mt-4 flex w-full items-center justify-center gap-2 rounded-2xl bg-[#5B3F22] px-5 py-3 font-bold text-[#EFE1D1] shadow-md transition hover:bg-[#432E1A]"
     >
-      <span> {Role ===3 ? "الانتقال لصفحة الادمن" : "الانتقال لصفحة الرحلات"}</span>
+      <span>
+        {isAdmin ? "الانتقال إلى صفحة الأدمن" : "الانتقال إلى صفحة الرحلات"}
+      </span>
     </Link>
   );
 }
